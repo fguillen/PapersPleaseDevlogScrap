c953380dbf27373715d179144580fda5e2fab07e
Fernando Guillen <fguillen.mail@gmail.com>
2022-08-09 21:19:11 +0200

    blockquote style

--- script.rb
+++ script.rb
@@ -2,23 +2,42 @@ require "httparty"
 require "nokogiri"
 require "erb"
 
-
 class Script
   TEMPLATE_PATH_INDEX = "#{__dir__}/templates/index.erb"
-
   RESULT_PATH = "#{__dir__}/index.html"
   CACHE_PATH = "#{__dir__}/cache.html"
+  BASE_URL = "https://forums.tigsource.com/index.php?topic=29750"
+  USE_CACHE = true
 
   def run
-    File.truncate(RESULT_PATH, 0)
+    pages = (0..700).step(20).to_a
 
+    articles = []
+
+    pages[0..2].each do |page|
+      articles.concat parse_page(page)
+    end
+
+    render_erb(articles)
+  end
+
+  def render_erb(articles)
+    # puts "articles: #{JSON.pretty_generate articles}"
     template_index = File.read(TEMPLATE_PATH_INDEX)
+    erb = ERB.new(template_index)
+    html = erb.result_with_hash(articles: articles)
+
+    File.open(RESULT_PATH, "w") { |f| f.write html }
+  end
+
+  def parse_page(page)
+    puts "Page: #{page}"
     html = nil
-    if(File.exists?(CACHE_PATH))
+    if(USE_CACHE && File.exists?(CACHE_PATH))
       puts "Using cache!"
       html = File.read(CACHE_PATH)
     else
-      response = HTTParty.get("https://forums.tigsource.com/index.php?topic=29750.0")
+      response = HTTParty.get("#{BASE_URL}.#{page}")
       html = response.body
       File.open(CACHE_PATH, "w") { |f| f.write html }
     end
@@ -40,12 +59,7 @@ class Script
       content = element.css("div.post")
       content = clean_content(content, doc)
 
-
-      html = "<h1>#{date}</h1>"
-      html += content.inner_html
-
-
-      puts "Date: #{date}"
+      puts "Id: #{id}"
 
       articles << {
         id: id,
@@ -55,10 +69,7 @@ class Script
       }
     end
 
-    erb = ERB.new(template_index)
-    html = erb.result_with_hash(articles: articles)
-
-    File.open(RESULT_PATH, "w") { |f| f.write html }
+    articles
   end
 
   def clean_date(date)
@@ -69,7 +80,7 @@ class Script
     content.css("div.quoteheader").remove
 
     content.css("div.quote").each do |div|
-      new_node = doc.create_element "blockquote"
+      new_node = doc.create_element("blockquote", { class: "blockquote" })
       new_node.inner_html = div.inner_html
       div.replace new_node
     end
